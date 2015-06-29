b37f405d4b83c3eaa1662fca46c74180c2790f0e
Kyznecov Andrey <andreykyz@gmail.com>
2015-06-29 14:10:38 +0300

    Revert "add dump_stack on disconnect #11"
    This reverts commit 6665d2766362c415690af2cd301c6983203c5df7.

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -45,7 +45,6 @@
 
 #include <linux/cryptohash.h>
 #include <linux/kconfig.h>
-#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/netpoll.h>
 #include <linux/list.h>
@@ -1290,7 +1289,6 @@ void mptcp_del_sock(struct sock *sk)
 	mptcp_debug("%s: Removing subsock tok %#x pi:%d state %d is_meta? %d\n",
 		    __func__, mpcb->mptcp_loc_token, tp->mptcp->path_index,
 		    sk->sk_state, is_meta_sk(sk));
-	dump_stack();
 
 	if (tp_prev == tp) {
 		mpcb->connection_list = tp->mptcp->next;
@@ -1485,7 +1483,7 @@ void mptcp_sub_close(struct sock *sk, unsigned long delay)
 			return;
 		sock_put(sk);
 	}
-	mptcp_debug("%s: delay %d\n", __func__, delay);
+
 	if (!delay) {
 		unsigned char old_state = sk->sk_state;
 
@@ -1530,7 +1528,7 @@ void mptcp_sub_close(struct sock *sk, unsigned long delay)
 			sk->sk_state = old_state;
 		}
 	}
-	dump_stack();
+
 	sock_hold(sk);
 	queue_delayed_work(mptcp_wq, work, delay);
 }
